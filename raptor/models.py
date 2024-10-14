8b09a224032fb629f8b0f1db514b4785da55212a
mick <mickb.py@gmail.com>
2024-10-14 22:09:53 +0100

    Initial commit

---
+++ raptor/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class Sighting(models.Model):
+    class Status(models.TextChoices):
+        # names, values, labels
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    when = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-when']
+        indexes = [
+            models.Index(fields=['-when']),
+        ]
+
+    def __str__(self):
+        return self.title
